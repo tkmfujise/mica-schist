b6204ed7ff011658e1d3aa82360ebeca40594380
FUJISE Takumi <106900036+tkmfujise@users.noreply.github.com>
2026-05-16 02:17:07 +0900

    create mica

---
+++ mica/lib/mica/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Mica
+  VERSION = "0.1.0"
+end
---
+++ mica/spec/mica_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe Mica do
+  it "has a version number" do
+    expect(Mica::VERSION).not_to be nil
+  end
+end
