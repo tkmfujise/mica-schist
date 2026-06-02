eca478a85fbb3a94d278e1037715519a756f178f
FUJISE Takumi <106900036+tkmfujise@users.noreply.github.com>
2026-06-03 00:47:15 +0900

    add memo

---
+++ memos/generate_arpeggio.rb
@@ -0,0 +1,61 @@
+mica_dir = '/path/to/mica-sound/content/cc0/sonic_pi/arpeggio'
+
+chords = %i[
+  M
+  m
+  7
+  M7
+  m7
+  sus2
+  sus4
+  dim
+  augmented
+  5
+  add9
+  madd9
+  9
+  m9
+  maj9
+  11
+  13
+  m6
+  m7b5
+  dom7
+  5
+  sus2
+  sus4
+  add9
+  madd9
+  augmented
+]
+
+
+path = nil
+code = nil
+
+
+def generate(note, chord)
+  <<~RUBY.gsub(/^\s+/, '')
+  use_synth :piano
+  play :#{note}
+  sleep 0.25
+  play_pattern_timed chord(:#{note}, '#{chord}'), [0.25]
+  play_pattern_timed chord(:#{note}, '#{chord}'), [0.1]
+  RUBY
+end
+
+
+
+note = :C3
+
+chords.each do |chord|
+  dir = "#{mica_dir}/#{chord}_#{note}"
+  
+  path = "#{dir}/#{chord}_#{note}.rb"
+  # FileUtils.mkdir_p dir
+  code = generate(note, chord)
+  # File.write(path, code)
+end
+
+puts path
+puts code
