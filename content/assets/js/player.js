2b76694ef6735b8fa2de386682aee68f98835606
FUJISE Takumi <106900036+tkmfujise@users.noreply.github.com>
2026-06-02 22:07:00 +0900

    Add shuffle button

--- content/assets/js/player.js
+++ content/assets/js/player.js
@@ -1,9 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const audio   = document.querySelector('audio#main-audio')
-  const history = document.querySelector('#play-history .container')
-  const label   = document.querySelector('#play-history .current')
-  const buttons = Array.from(document.querySelectorAll('button.audio-item'))
-  const audioWrapperSelector = 'tr.audio-list'
+  const selectorName = {
+    mainAudio:        'audio#main-audio', 
+    historyContainer: '#play-history .container',
+    currentLabel:     '#play-history .current',
+    audioButton:      'button.audio-item',
+    audioList:        '.audio-list',
+    shuffleButton:    'button#shuffle-btn',
+    audioWrapper:     'tr.audio-list',
+  }
+  const audio     = document.querySelector(selectorName.mainAudio)
+  const history   = document.querySelector(selectorName.historyContainer)
+  const label     = document.querySelector(selectorName.currentLabel)
+  const buttons   = Array.from(document.querySelectorAll(selectorName.audioButton))
+  const audioList = Array.from(document.querySelectorAll(selectorName.audioList))
+  const shuffleButton = document.querySelector(selectorName.shuffleButton)
   const playedSrc = []
 
   const clearPlaying = () => {
@@ -18,7 +28,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const addHistory = (btn) => {
     if (playedSrc.includes(btn.dataset.src)) return
     playedSrc.push(btn.dataset.src)
-    const wrapper = btn.closest(audioWrapperSelector).cloneNode(true)
+    const wrapper = btn.closest(selectorName.audioWrapper).cloneNode(true)
     const newBtn  = wrapper.querySelector('button')
     setCallbacks(newBtn)
     history.prepend(wrapper)
@@ -47,7 +57,26 @@ document.addEventListener("DOMContentLoaded", () => {
     addHistory(btn)
   }
 
+
+  const shuffleArray = (arr) => {
+    for (let i = arr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1))
+      ;[arr[i], arr[j]] = [arr[j], arr[i]]
+    }
+  }
+
+
+  const shuffleAndPlay = () => {
+    const container = audioList[0].parentElement
+    shuffleArray(audioList)
+    audioList.forEach(e => container.appendChild(e))
+    const btn = audioList[0].querySelector(selectorName.audioButton)
+    if (btn) play(btn)
+  }
+
+
   audio.addEventListener('ended', clearPlaying)
+  shuffleButton.addEventListener('click', shuffleAndPlay)
 
   buttons.forEach(btn => setCallbacks(btn))
 })
