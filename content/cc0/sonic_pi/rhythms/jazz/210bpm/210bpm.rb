b903fa835f22d43efda7a7efc71a34b5ba0b6390
FUJISE Takumi <106900036+tkmfujise@users.noreply.github.com>
2026-06-06 00:43:56 +0900

    Fix jazz. Add 120 bpm - 240 bpm

---
+++ content/cc0/sonic_pi/rhythms/jazz/210bpm/210bpm.rb
@@ -0,0 +1,18 @@
+# Jazz (210 bpm)
+# max-recording-time: 5
+ride  = (ring 1, 0, 0,  0.6, 0, 0.8)
+kick  = (ring 0, 0, 0,  0.4, 0, 0)
+
+use_bpm 210
+
+define :jazz do
+  tick
+  sample :drum_cymbal_soft, amp: ride.look if ride.look > 0
+  sample :drum_bass_soft,   amp: kick.look if kick.look > 0
+  sleep 1.0/3
+end
+
+
+(ride.length * 4).times do
+  jazz
+end
--- memos/generate_rhythm/jazz.rb
+++ memos/generate_rhythm/jazz.rb
@@ -6,7 +6,7 @@ def generate(bpm)
   ring_length = 6
   <<~RUBY
     # Jazz (#{bpm} bpm)
-    # max-recording-time: #{(60.0/bpm * ring_length * 1.0/2/3 * 4).ceil + 2}
+    # max-recording-time: #{(60.0/bpm * ring_length * 1.0/3 * 4).ceil + 2}
     ride  = (ring 1, 0, 0,  0.6, 0, 0.8)
     kick  = (ring 0, 0, 0,  0.4, 0, 0)
 
@@ -16,7 +16,7 @@ def generate(bpm)
       tick
       sample :drum_cymbal_soft, amp: ride.look if ride.look > 0
       sample :drum_bass_soft,   amp: kick.look if kick.look > 0
-      sleep 1.0/2/3
+      sleep 1.0/3
     end
 
 
@@ -30,7 +30,7 @@ end
 path = nil
 code = nil
 
-[40, 50, 60, 70, 80, 90, 100, 110, 120].each do |bpm|
+(120..240).step(10).each do |bpm|
   dir = "#{mica_dir}/jazz/#{bpm}bpm"
   
   path = "#{dir}/#{bpm}bpm.rb"
