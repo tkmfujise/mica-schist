f099761cb5259a2dc479e02dd58035c70c69fb95
FUJISE Takumi <106900036+tkmfujise@users.noreply.github.com>
2026-06-06 17:38:11 +0900

    Add Bossa Nova

---
+++ content/cc0/sonic_pi/rhythms/bossa_nova/130bpm/130bpm.rb
@@ -0,0 +1,20 @@
+# Samba (130 bpm)
+# max-recording-time: 9
+# Bossa Nova
+hat   = (ring 1,1,1,1, 1,1,1,1, 1,1,1,1, 1,1,1,1)
+snare = (ring 0,0,1,0, 0,1,0,0, 1,0,0,1, 0,0,1,0)
+kick  = (ring 1,0,0,1, 1,0,0,1, 1,0,0,1, 1,0,0,1)
+
+use_bpm 130
+
+define :bossa_nova do
+  tick
+  sample :drum_cymbal_closed, amp: 0.5 if hat.look == 1
+  sample :drum_snare_soft,    amp: 1.5 if snare.look == 1
+  sample :drum_bass_soft,     amp: 2.0 if kick.look == 1
+  sleep 0.5
+end
+
+(hat.length * 2).times do
+  bossa_nova
+end
