4aca70c7e7b9ffa2c504c21acd5874d9bad66d69
FUJISE Takumi <106900036+tkmfujise@users.noreply.github.com>
2026-06-06 16:22:28 +0900

    Add samba

---
+++ content/cc0/sonic_pi/rhythms/samba/70bpm/70bpm.rb
@@ -0,0 +1,19 @@
+# Samba (70 bpm)
+# max-recording-time: 8
+hat   = (ring 1,0,1,1, 1,0,1,1, 1,0,1,1, 1,0,1,1)
+snare = (ring 0,0,1,0, 0,1,0,0, 1,0,0,1, 0,0,1,0)
+kick  = (ring 1,0,0,1, 1,0,0,1, 1,0,0,1, 1,0,0,1)
+
+use_bpm 70
+
+define :samba do
+  tick
+  sample :drum_cymbal_closed, amp: 0.4 if hat.look == 1
+  sample :drum_snare_soft,    amp: 0.8 if snare.look == 1
+  sample :drum_bass_soft,     amp: 1.2 if kick.look == 1
+  sleep 0.25
+end
+
+(hat.length * 2).times do
+  samba
+end
---
+++ memos/generate_rhythm/samba.rb
@@ -0,0 +1,46 @@
+
+mica_dir = '/path/to/mica-sound/content/cc0/sonic_pi/rhythms'
+
+
+def generate(bpm)
+  ring_length = 16
+  <<~RUBY
+    # Samba (#{bpm} bpm)
+    # max-recording-time: #{(60.0/bpm * ring_length * 0.25 * 2).ceil + 1}
+    hat   = (ring 1,0,1,1, 1,0,1,1, 1,0,1,1, 1,0,1,1)
+    snare = (ring 0,0,1,0, 0,1,0,0, 1,0,0,1, 0,0,1,0)
+    kick  = (ring 1,0,0,1, 1,0,0,1, 1,0,0,1, 1,0,0,1)
+
+    use_bpm #{bpm}
+
+    define :samba do
+      tick
+      sample :drum_cymbal_closed, amp: 0.4 if hat.look == 1
+      sample :drum_snare_soft,    amp: 0.8 if snare.look == 1
+      sample :drum_bass_soft,     amp: 1.2 if kick.look == 1
+      sleep 0.25
+    end
+
+    (hat.length * 2).times do
+      samba
+    end
+  RUBY
+end
+
+
+path = nil
+code = nil
+
+(60..130).step(10).each do |bpm|
+  dir = "#{mica_dir}/samba/#{bpm}bpm"
+  
+  path = "#{dir}/#{bpm}bpm.rb"
+  FileUtils.mkdir_p dir
+  code = generate(bpm)
+  File.write(path, code)
+end
+
+puts path
+puts code
+
+
