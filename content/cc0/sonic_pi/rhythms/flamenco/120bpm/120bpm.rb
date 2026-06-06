5205b6f43d15c56fbb26e741d4db7c62fd1c4b47
FUJISE Takumi <106900036+tkmfujise@users.noreply.github.com>
2026-06-06 18:45:43 +0900

    Add Flamenco rhythm

---
+++ content/cc0/sonic_pi/rhythms/flamenco/120bpm/120bpm.rb
@@ -0,0 +1,19 @@
+# Flamenco (120 bpm)
+# max-recording-time: 5
+hat   = (ring 1,1, 0,0, 1,1, 0,1)
+snare = (ring 0,0, 1,1, 0,0, 1,0)
+kick  = (ring 1,0, 0,1, 0,0, 1,0)
+
+use_bpm 120
+
+define :flamenco do
+  tick
+  sample :drum_cymbal_closed, amp: 0.3 if hat.look == 1
+  sample :drum_snare_soft,    amp: 1.5 if snare.look == 1
+  sample :drum_heavy_kick,    amp: 0.8 if kick.look == 1
+  sleep 0.25
+end
+
+(hat.length * 4).times do
+  flamenco
+end
