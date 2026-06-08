d66d23cc08cc14ac0800a84e954693b892560d4e
FUJISE Takumi <106900036+tkmfujise@users.noreply.github.com>
2026-06-08 21:05:08 +0900

    Add rumba, remove flamenco

---
+++ content/cc0/sonic_pi/rhythms/rumba/130bpm/130bpm.rb
@@ -0,0 +1,21 @@
+# Rumba (130 bpm)
+# max-recording-time: 5
+hat    = (ring 1,0,0, 1,0,0, 1,0)
+palma1 = (ring 0,0,1,0, 1,0, 1,0)
+palma2 = (ring 0,1,1,0, 1,1, 0,0)
+palma  = palma1 + palma2
+kick   = (ring 1,0,0,0, 1,0, 0,0)
+
+use_bpm 130
+
+define :rumba do
+  tick
+  synth :sc808_maracas, decay: 10, amp: 0.4 if hat.look == 1
+  synth :sc808_clap,               amp: 1.5 if palma.look == 1
+  sample :drum_heavy_kick,         amp: 1.2 if kick.look == 1
+  sleep 0.25
+end
+
+(hat.length * 4).times do
+  rumba
+end
---
+++ memos/generate_rhythm/rumba.rb
@@ -0,0 +1,47 @@
+
+mica_dir = '../content/cc0/sonic_pi/rhythms'
+
+
+def generate(bpm)
+  ring_length = 8
+  <<~RUBY
+    # Rumba (#{bpm} bpm)
+    # max-recording-time: #{(60.0/bpm * ring_length * 0.25 * 4).ceil + 1}
+    hat    = (ring 1,0,0, 1,0,0, 1,0)
+    palma1 = (ring 0,0,1,0, 1,0, 1,0)
+    palma2 = (ring 0,1,1,0, 1,1, 0,0)
+    palma  = palma1 + palma2
+    kick   = (ring 1,0,0,0, 1,0, 0,0)
+
+    use_bpm #{bpm}
+
+    define :rumba do
+      tick
+      synth :sc808_maracas, decay: 10, amp: 0.4 if hat.look == 1
+      synth :sc808_clap,               amp: 1.5 if palma.look == 1
+      sample :drum_heavy_kick,         amp: 1.2 if kick.look == 1
+      sleep 0.25
+    end
+
+    (hat.length * 4).times do
+      rumba
+    end
+  RUBY
+end
+
+
+path = nil
+code = nil
+
+(60..130).step(10).each do |bpm|
+  dir = "#{mica_dir}/rumba/#{bpm}bpm"
+  
+  path = "#{dir}/#{bpm}bpm.rb"
+  FileUtils.mkdir_p dir
+  code = generate(bpm)
+  File.write(path, code)
+end
+
+puts path
+puts code
+
