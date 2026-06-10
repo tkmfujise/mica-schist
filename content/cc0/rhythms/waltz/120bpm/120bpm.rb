7ebfbb044e9a6abfe8c5efd6b71c248e6957ae19
FUJISE Takumi <106900036+tkmfujise@users.noreply.github.com>
2026-06-11 00:30:41 +0900

    Add Waltz

---
+++ content/cc0/rhythms/waltz/120bpm/120bpm.rb
@@ -0,0 +1,21 @@
+# Waltz (120 bpm)
+# max-recording-time: 7
+hat   = (ring 0,1,1)
+snare = (ring 0,1,1)
+kick  = (ring 1,0,0)
+s_amp = (ring 0, 0.8, 0.4)
+
+use_bpm 120
+
+define :waltz do
+  tick
+  sample :drum_cymbal_closed, amp: 0.4        if hat.look == 1
+  sample :drum_snare_soft,    amp: s_amp.look if snare.look == 1
+  sample :drum_heavy_kick,    amp: 0.7        if kick.look == 1
+
+  sleep 1.0
+end
+
+(snare.length * 4).times do
+  waltz
+end
---
+++ memos/generate_rhythm/waltz.rb
@@ -0,0 +1,48 @@
+
+mica_dir = '../content/cc0/rhythms'
+
+
+def generate(bpm)
+  ring_length = 3
+  <<~RUBY
+    # Waltz (#{bpm} bpm)
+    # max-recording-time: #{(60.0/bpm * ring_length * 1.0 * 4).ceil + 1}
+    hat   = (ring 0,1,1)
+    snare = (ring 0,1,1)
+    kick  = (ring 1,0,0)
+    s_amp = (ring 0, 0.8, 0.4)
+
+    use_bpm #{bpm}
+
+    define :waltz do
+      tick
+      sample :drum_cymbal_closed, amp: 0.4        if hat.look == 1
+      sample :drum_snare_soft,    amp: s_amp.look if snare.look == 1
+      sample :drum_heavy_kick,    amp: 0.7        if kick.look == 1
+  
+      sleep 1.0
+    end
+
+    (snare.length * 4).times do
+      waltz
+    end
+  RUBY
+end
+
+
+path = nil
+code = nil
+
+(100..180).step(10).each do |bpm|
+  dir = "#{mica_dir}/waltz/#{bpm}bpm"
+  
+  path = "#{dir}/#{bpm}bpm.rb"
+  FileUtils.mkdir_p dir
+  code = generate(bpm)
+  File.write(path, code)
+end
+
+puts path
+puts code
+
+
